b2c7cd8800823a62a0aa93e14179ddae08a18b1c
Itai Baz <itaib@mellanox.com>
2015-03-17 16:24:49 -0700

    Update license

--- SwitchX-2/source/sx_api_dbg.h
+++ SwitchX-2/source/sx_api_dbg.h
@@ -1,14 +1,18 @@
 /*
- *                  - Mellanox Proprietary -
+ *  Copyright (C) 2014. Mellanox Technologies, Ltd. ALL RIGHTS RESERVED.
  *
- *  Copyright (C) January 2010, Mellanox Technologies Ltd.  ALL RIGHTS RESERVED.
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *    not use this file except in compliance with the License. You may obtain
+ *    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Except as specifically permitted herein, no portion of the information,
- *  including but not limited to object code and source code, may be reproduced,
- *  modified, distributed, republished or otherwise exploited in any form or by
- *  any means for any purpose without the prior written permission of Mellanox
- *  Technologies Ltd. Use of software subject to the terms and conditions
- *  detailed in the file "LICENSE.txt".
+ *    THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ *    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ *    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ *    FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ *    See the Apache Version 2.0 License for specific language governing
+ *    permissions and limitations under the License.
+ *
  */
 
 #ifndef __SX_API_DBG_H__
